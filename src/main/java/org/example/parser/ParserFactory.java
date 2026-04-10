f7194732812f144da4c7fa104fad0cc922ef2b6d
Sophia <sondanu@yandex.ru>
2026-04-10 17:31:56 +0300

    Update ParserFactory.java

--- src/main/java/org/example/parser/ParserFactory.java
+++ src/main/java/org/example/parser/ParserFactory.java
@@ -4,19 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParserFactory {
-    private static final Map<String, IMissionParser> parsers = new HashMap<>();
-
-    static {
-        parsers.put("json", new JsonMissionParser());
-        parsers.put("xml", new XmlMissionParser());
-        parsers.put("txt", new TxtMissionParser());
-        parsers.put("yaml", new YamlMissionParser());
-        parsers.put("a5", new A5MissionParser());
-    }
+    private static Map<String, IMissionParser> parsers = Map.of(
+            "json", new JsonMissionParser(),
+            "xml",  new XmlMissionParser(),
+            "txt",  new TxtMissionParser(),
+            "yaml", new YamlMissionParser(),
+            "a5",   new A5MissionParser()
+    );
 
     public static IMissionParser getParser(String fileName) {
         if (fileName.contains("Mission A5")) return parsers.get("a5");
         String ext = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
-        return parsers.getOrDefault(ext, parsers.get("txt"));
+        return parsers.get(ext);
     }
 }
